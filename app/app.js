d94c7be76e86749c9a77e0b000650462d62c231d
fabio garces <fagarces06@gmail.com>
2023-06-25 12:39:37 -0500

    inicio proyecto

---
+++ app/app.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+
+
+export default function App () {
+   
+      return (
+            <div>
+                  hola react
+
+            </div>
+      )
+}
